8b6aed9574790f02b5bae2154cbfe40a3e370887
Sebastiaán Arriaga <sebastian.arriaga@uabc.edu.mx>
2025-01-27 12:51:06 -0800

    Scope changed

--- src/WebSocketManager.js
+++ src/WebSocketManager.js
@@ -1,55 +1,57 @@
-class WebSocketManager {
-    constructor(port = 51510) {
-        this.port = port;
-        this.socket = null;
-    }
-
-    connect() {
-        this.socket = new WebSocket(`ws://localhost:${this.port}`);
-
-        this.socket.onopen = () => {
-            console.log('Connected to WebSocket server');
-        };
-
-        this.socket.onmessage = (event) => {
-            const message = JSON.parse(event.data);
-            console.log('Message from server:', message);
-
-            this.handleMessage(message);
-        };
-
-        this.socket.onerror = (error) => {
-            console.error('WebSocket error:', error);
-        };
-
-        this.socket.onclose = () => {
-            console.log('WebSocket connection closed');
-        };
-    }
-
-    sendMessage(message) {
-        if (!this.socket || this.socket.readyState !== WebSocket.OPEN) {
-            console.error('WebSocket is not connected');
-            return;
+(function () {
+    class WebSocketManager {
+        constructor(port = 51510) {
+            this.port = port;
+            this.socket = null;
         }
-        this.socket.send(JSON.stringify(message));
-    }
 
-    handleMessage(message) {
-        console.log('Handling message:', message);
+        connect() {
+            this.socket = new WebSocket(`ws://localhost:${this.port}`);
 
-        if (message.type === 'printStatus') {
-            console.log('Print status:', message.data);
-        } else if (message.type === 'portMessageStatus') {
-            console.log('Port message status:', message.data);
-        } else if (message.type === 'encryptedToken') {
-            console.log('Encrypted token from server:', message.data);
+            this.socket.onopen = () => {
+                console.log('Connected to WebSocket server');
+            };
+
+            this.socket.onmessage = (event) => {
+                const message = JSON.parse(event.data);
+                console.log('Message from server:', message);
+
+                this.handleMessage(message);
+            };
+
+            this.socket.onerror = (error) => {
+                console.error('WebSocket error:', error);
+            };
+
+            this.socket.onclose = () => {
+                console.log('WebSocket connection closed');
+            };
+        }
+
+        sendMessage(message) {
+            if (!this.socket || this.socket.readyState !== WebSocket.OPEN) {
+                console.error('WebSocket is not connected');
+                return;
+            }
+            this.socket.send(JSON.stringify(message));
+        }
+
+        handleMessage(message) {
+            console.log('Handling message:', message);
+
+            if (message.type === 'printStatus') {
+                console.log('Print status:', message.data);
+            } else if (message.type === 'portMessageStatus') {
+                console.log('Port message status:', message.data);
+            } else if (message.type === 'encryptedToken') {
+                console.log('Encrypted token from server:', message.data);
+            }
+        }
+
+        disconnect() {
+            this.socket.close();
         }
     }
 
-    disconnect() {
-        this.socket.close();
-    }
-}
-
-window.WebSocketManager = WebSocketManager;
+    window.WebSocketManager = WebSocketManager;
+})();
--- src/index.js
+++ src/index.js
@@ -1,30 +1,32 @@
-class Bridge {
-    constructor(port = 51510) {
-        this.webSocketManager = new window.WebSocketManager(port);
+(function () {
+    class Bridge {
+        constructor(port = 51510) {
+            this.webSocketManager = new window.WebSocketManager(port);
+        }
+
+        connect() {
+            this.webSocketManager.connect();
+        }
+
+        sendKey(publicKey) {
+            const message = {type: "publicKey", data: publicKey};
+            this.webSocketManager.sendMessage(message);
+        }
+
+        print(configs, html)  {
+            const message = { type: 'printFile', data: { selectedConfigs: configs, htmlContent: html } };
+            this.webSocketManager.sendMessage(message);
+        }
+
+        sendToDisplay(var1, var2) {
+            const message = { type: 'portMessage', data: { messageA: var1, messageB: var2 } };
+            this.webSocketManager.sendMessage(message);
+        }
+
+        disconnect() {
+            this.webSocketManager.disconnect();
+        }
     }
 
-    connect() {
-        this.webSocketManager.connect();
-    }
-
-    sendKey(publicKey) {
-        const message = {type: "publicKey", data: publicKey};
-        this.webSocketManager.sendMessage(message);
-    }
-
-    print(configs, html)  {
-        const message = { type: 'printFile', data: { selectedConfigs: configs, htmlContent: html } };
-        this.webSocketManager.sendMessage(message);
-    }
-
-    sendToDisplay(var1, var2) {
-        const message = { type: 'portMessage', data: { messageA: var1, messageB: var2 } };
-        this.webSocketManager.sendMessage(message);
-    }
-
-    disconnect() {
-        this.webSocketManager.disconnect();
-    }
-}
-
-window.Bridge = Bridge;
+    window.Bridge = Bridge;
+})();
