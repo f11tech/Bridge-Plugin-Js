b8c107497d0e1f06f372f0f21e708fb81eecb91a
Juan Elías López <juaneliaslopez@gmail.com>
2026-02-11 21:35:49 -0800

    Changes for handling callbacks

--- src/WebSocketManager.js
+++ src/WebSocketManager.js
@@ -1,19 +1,26 @@
 import { decryptData } from "./Decryption";
+import { v4 as uuidv4 } from 'uuid';
 
 (function () {
     class WebSocketManager {
+
+        #callbacks = {};
+
         constructor(port = 51510) {
             this.port = port;
             this.socket = null;
         }
 
-        connect() {
+        connect(onSuccess, onError, onClose) {
             try {
                 this.socket = new WebSocket(`ws://localhost:${this.port}`);
 
                 this.socket.onopen = () => {
                     console.log("Connected to WebSocket server");
                     this.dispatchStatus("connected");
+                    if (typeof onSuccess === 'function') {
+                        onSuccess();
+                    }
                 };
 
                 this.socket.onmessage = (event) => {
@@ -29,11 +36,17 @@ import { decryptData } from "./Decryption";
                 this.socket.onerror = (error) => {
                     console.error("WebSocket error:", error);
                     this.dispatchStatus("connection_failed");
+                    if (typeof onError === 'function') {
+                        onError();
+                    }
                 };
 
                 this.socket.onclose = () => {
                     console.log("WebSocket connection closed");
                     this.dispatchStatus("disconnected");
+                    if (typeof onClose === 'function') {
+                        onClose();
+                    }
                 };
 
                 return { success: true, message: "Connecting..." };
@@ -43,16 +56,22 @@ import { decryptData } from "./Decryption";
             }
         }
 
-        sendMessage(message) {
+        sendMessage(message, onSuccess, onError) {
             if (!this.socket || this.socket.readyState !== WebSocket.OPEN) {
                 console.error("WebSocket is not connected");
                 return { success: false, message: "WebSocket is not connected" };
             }
 
+            message.uuid = uuidv4();
+            this.setCallbacks(message.uuid, onSuccess, onError);
+
             try {
                 this.socket.send(JSON.stringify(message));
                 return { success: true, message: "Message sent successfully" };
             } catch (error) {
+
+                this.executeCallbacks(message.uuid, false);
+
                 console.error("Error sending message:", error);
                 return { success: false, message: "Error sending message" };
             }
@@ -64,12 +83,15 @@ import { decryptData } from "./Decryption";
                 if (message.type === "encryptedToken") {
                     const decrypted = decryptData(message.data);
                     console.log("Decrypted data:", decrypted);
+                    this.executeCallbacks(message.uuid, message.status, decrypted);
                     this.dispatchStatus("decryption_success", decrypted);
                 } else if (message.type === "printStatus") {
                     console.log("Print status:", message.data);
+                    this.executeCallbacks(message.uuid, message.status);
                     this.dispatchStatus("print_status", message.data);
                 } else if (message.type === "portMessageStatus") {
                     console.log("Port message status:", message.data);
+                    this.executeCallbacks(message.uuid, message.status);
                     this.dispatchStatus("port_message_status", message.data);
                 }
             } catch (error) {
@@ -93,6 +115,45 @@ import { decryptData } from "./Decryption";
                 console.log("WebSocket disconnected");
             }
         }
+
+        setCallbacks(uuid, onSuccess, onError) {
+            let callbackPair = {},
+                hasCallback = false;
+
+            if (typeof onSuccess === 'function') {
+                callbackPair.onSuccess = onSuccess;
+                hasCallback = true;
+            }
+
+            if (typeof onError === 'function') {
+                callbackPair.onError = onError;
+                hasCallback = true;
+            }
+
+            if (!hasCallback) {
+                return;
+            }
+
+            this.#callbacks[uuid] = callbackPair;
+        }
+
+        executeCallbacks(uuid, isSuccess, data) {
+            let callbackPair = this.#callbacks[uuid] ?? null;
+            if (callbackPair === null) {
+                return;
+            }
+
+            if (isSuccess && (typeof callbackPair.onSuccess === 'function')) {
+                callbackPair.onSuccess(data);
+            }
+
+            if (!isSuccess && (typeof callbackPair.onError === 'function')) {
+                callbackPair.onError(data);
+            }
+
+            delete this.#callbacks[uuid];
+        }
+
     }
 
     window.WebSocketManager = WebSocketManager;
--- src/index.js
+++ src/index.js
@@ -5,53 +5,27 @@
             this.webSocketManager = new window.WebSocketManager(port);
         }
 
-        connect(callbackSuccess, callbackError) {
-            const result = this.webSocketManager.connect();
-            if (result.success && typeof callbackSuccess === "function") {
-                callbackSuccess(result.message);
-            } else if (typeof callbackError === "function") {
-                callbackError(result.message);
-            }
+        connect(onSuccess, onError, onClose) {
+            return this.webSocketManager.connect(onSuccess, onError, onClose);
         }
 
-        sendKey(publicKey, callbackSuccess, callbackError) {
+        sendKey(publicKey, onSuccess, onError) {
             const message = { type: "publicKey", data: publicKey };
-            const result = this.webSocketManager.sendMessage(message);
-            if (result.success && typeof callbackSuccess === "function") {
-                callbackSuccess(result.message);
-            } else if (typeof callbackError === "function") {
-                callbackError(result.message);
-            }
+            this.webSocketManager.sendMessage(message, onSuccess, onError);
         }
 
-        print(configs, html, callbackSuccess, callbackError) {
+        print(configs, html, onSuccess, onError) {
             const message = { type: 'printFile', data: { selectedConfigs: configs, htmlContent: html } };
-            const result = this.webSocketManager.sendMessage(message);
-            if (result.success && typeof callbackSuccess === "function") {
-                callbackSuccess(result.message);
-            } else if (typeof callbackError === "function") {
-                callbackError(result.message);
-            }
+            this.webSocketManager.sendMessage(message, onSuccess, onError);
         }
 
-        sendToDisplay(var1, var2, port, callbackSuccess, callbackError) {
+        sendToDisplay(var1, var2, port, onSuccess, onError) {
             const message = { type: 'portMessage', data: { messageA: var1, messageB: var2, selectedPort: port } };
-            const result = this.webSocketManager.sendMessage(message);
-            if (result.success && typeof callbackSuccess === "function") {
-                callbackSuccess(result.message);
-            } else if (typeof callbackError === "function") {
-                callbackError(result.message);
-            }
+            this.webSocketManager.sendMessage(message, onSuccess, onError);
         }
 
-        disconnect(callbackSuccess, callbackError) {
+        disconnect() {
             this.webSocketManager.disconnect();
-            const result = { success: true, message: "Disconnected successfully" };
-            if (result.success && typeof callbackSuccess === "function") {
-                callbackSuccess(result.message);
-            } else if (typeof callbackError === "function") {
-                callbackError(result.message);
-            }
         }
     }
 
